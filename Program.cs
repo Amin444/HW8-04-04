644f296dc681aca6494c5aced2b78f1928568d70
unknown <aminmirzoev123@gmail.com>
2020-04-06 23:03:05 +0500

    task 1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW8_06_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Добро пожаловать в Alif Convertetion Bank:");
+            System.Console.Write("Введите сколько сомони вы хотите конвертировать:");
+            double Som=double.Parse(Console.ReadLine());
+                string currency=" ";
+                    switch(currency)
+                    {
+                            case "USD":
+                    }
+        }
+    }
+        class Converter
+        {
+            public double USD{get;set;}
+            public double EUR{get;set;}
+            public double RUB{get;set;}
+            public Converter()
+            {
+                this.USD=USD;
+                this.EUR=EUR;
+                this.RUB=RUB;
+                
+            }
+        }
+
+}
+
